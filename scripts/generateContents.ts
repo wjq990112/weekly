069984ad16203504ea95c1769b53d6834108add3
wjq990112 <1163585385@qq.com>
2021-11-13 22:23:29 +0800

    Add GitHub Actions

---
+++ scripts/generateContents.ts
@@ -0,0 +1,22 @@
+import fs from 'fs';
+
+const README = fs.readFileSync('README.md', { encoding: 'utf-8' });
+
+const CONTENTS = '## Contents\n';
+
+const contentsIdx = README.indexOf(CONTENTS);
+
+let READMEWithoutContents = README.slice(0, contentsIdx + CONTENTS.length);
+
+READMEWithoutContents += '\n';
+
+const weeklies = fs.readdirSync('weekly');
+
+weeklies.forEach((weekly) => {
+  READMEWithoutContents += `- [淘系前端架构 - 周刊 - ${weekly.slice(
+    9,
+    -3
+  )} 期](weekly/${encodeURIComponent(weekly)})\n`;
+});
+
+fs.writeFileSync('README.md', READMEWithoutContents);
